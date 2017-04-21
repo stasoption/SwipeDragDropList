fae19283f4efc5e27300d459d9111e2f7c3a33c2
Stas Averin <averin.developer@gmail.com>
2017-04-21 14:26:56 +0300

    added interface for direction

--- app/src/main/java/ru/a3technology/swipedraglist/MainActivity.java
+++ app/src/main/java/ru/a3technology/swipedraglist/MainActivity.java
@@ -6,11 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 import ru.a3technology.swipedraglist.drager.DragDropAdapter;
 import ru.a3technology.swipedraglist.drager.DragTouchHelper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DragDropAdapter.OnSwipeDragDropDirection {
 
     private Context mContext;
 
@@ -20,14 +24,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mContext = MainActivity.this;
 
+        String[] STRINGS = new String[]{
+            "I am FIRST item", "I am SECOND item", "I am THIRD item", "I am FOURTH item", "I am FIFTH item"
+    };
+
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
 
-        DragDropAdapter adapter = new DragDropAdapter();
+        DragDropAdapter adapter = new DragDropAdapter(Arrays.asList(STRINGS));
+        adapter.setOnSwipeDragDropDirection(this);
         ItemTouchHelper.Callback callback = new DragTouchHelper(adapter);
         ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
         touchHelper.attachToRecyclerView(recyclerView);
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClickItem() {
+        Log.e("onClickItem", "onClickItem");
+    }
+
+    @Override
+    public void onItemMoved(int fromPosition, int toPosition) {
+        Log.e("onItemMoved", fromPosition + " " + toPosition);
+    }
+
+    @Override
+    public void onActionLeftButton() {
+        Log.e("onActionLeftButton", "onActionLeftButton");
+    }
+
+    @Override
+    public void onActionCenterButton() {
+        Log.e("onActionCenterButton", "onActionCenterButton");
+    }
+
+    @Override
+    public void onActionRightButton() {
+        Log.e("onActionRightButton", "onActionRightButton");
     }
 }
--- app/src/main/java/ru/a3technology/swipedraglist/drager/DragDropAdapter.java
+++ app/src/main/java/ru/a3technology/swipedraglist/drager/DragDropAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -28,14 +29,18 @@ import ru.a3technology.swipedraglist.swiper.SwipeLayout;
 public class DragDropAdapter extends RecyclerView.Adapter<DragDropAdapter.ViewHolder>
         implements OnDragDropListener, SwipeItemMangerInterface, SwipeAdapterInterface {
 
+    //interface for list direction
+    private OnSwipeDragDropDirection mDirection;
+
     //manager for items direction when swiping
     private SwipeItemManager mSwipeManager = new SwipeItemManager(this);
+    private List<String> mItems = new ArrayList<>();
 
-    private final List<String> mItems = new ArrayList<>();
 
-    private static final String[] STRINGS = new String[]{
-            "I am FIRST item", "I am SECOND item", "I am THIRD item", "I am FOURTH item", "I am FIFTH item"
-    };
+
+    public void setOnSwipeDragDropDirection(OnSwipeDragDropDirection direction){
+        this.mDirection = direction;
+    }
 
     class ViewHolder extends RecyclerView.ViewHolder{
         CardView mCardView;
@@ -43,18 +48,24 @@ public class DragDropAdapter extends RecyclerView.Adapter<DragDropAdapter.ViewHo
         LinearLayout bottom_wrapper;
         TextView textView;
 
+        ImageView action_1, action_2, action_3;
+
         ViewHolder(View itemView) {
             super(itemView);
             mCardView = (CardView)itemView.findViewById(R.id.cardView);
             mSwipeLayout = (SwipeLayout)itemView.findViewById(R.id.mSwipeLayout);
             bottom_wrapper = (LinearLayout)itemView.findViewById(R.id.bottom_wrapper);
             textView = (TextView) itemView.findViewById(R.id.text);
+
+            action_1 = (ImageView) itemView.findViewById(R.id.action_1);
+            action_2 = (ImageView) itemView.findViewById(R.id.action_2);
+            action_3 = (ImageView) itemView.findViewById(R.id.action_3);
         }
     }
 
 
-    public DragDropAdapter() {
-        mItems.addAll(Arrays.asList(STRINGS));
+    public DragDropAdapter(List<String> items) {
+        mItems = new ArrayList<>(items);
         mSwipeManager.setMode(Attributes.Mode.Single);
     }
 
@@ -71,10 +82,38 @@ public class DragDropAdapter extends RecyclerView.Adapter<DragDropAdapter.ViewHo
             mSwipeManager.bind(holder.mSwipeLayout, position);
 
             holder.textView.setText(mItems.get(position));
+
             holder.mCardView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     mSwipeManager.closeAllItems();
+
+                    /*user click on the item of the list*/
+                    if(mDirection!=null)mDirection.onClickItem();
+                }
+            });
+
+            holder.action_1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    /*user click on the left button of the BottomView*/
+                    if(mDirection!=null)mDirection.onActionLeftButton();
+                }
+            });
+
+            holder.action_2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    /*user click on the left button of the BottomView*/
+                    if(mDirection!=null)mDirection.onActionCenterButton();
+                }
+            });
+
+            holder.action_3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    /*user click on the left button of the BottomView*/
+                    if(mDirection!=null)mDirection.onActionRightButton();
                 }
             });
         } catch (Exception mE) {
@@ -102,6 +141,7 @@ public class DragDropAdapter extends RecyclerView.Adapter<DragDropAdapter.ViewHo
                 Collections.swap(mItems, i, i - 1);
             }
         }
+        if(mDirection!=null)mDirection.onItemMoved(fromPosition, toPosition);
         /*updating the RecyclerView*/
         notifyItemMoved(fromPosition, toPosition);
         return true;
@@ -168,5 +208,12 @@ public class DragDropAdapter extends RecyclerView.Adapter<DragDropAdapter.ViewHo
         mSwipeManager.setMode(mode);
     }
 
+    public interface OnSwipeDragDropDirection{
+        void onClickItem();
+        void onItemMoved(int fromPosition, int toPosition);
+        void onActionLeftButton();
+        void onActionCenterButton();
+        void onActionRightButton();
+    }
 
 }
